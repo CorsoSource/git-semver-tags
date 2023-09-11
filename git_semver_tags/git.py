9312c1063bf3048c430f145b55353a2f3dd6ea9f
null-directory <dev@null.directory>
2023-09-11 00:15:55 -0400

    Added a datetime check to make it easier to compare against timestamps

--- git_semver_tags/git.py
+++ git_semver_tags/git.py
@@ -31,4 +31,23 @@ def highest_tagged_git_version(repo_location):
     return None
 
 
+def tag_datetime(repo_location, tag):
+    """Ask the repo for the date the tag was created"""
 
+    with Popen(
+            ['git', 'tag', '--list', tag, '--format=%(taggerdate:unix)'],
+            stdin=PIPE, stdout=PIPE, stderr=PIPE,
+            cwd=os.path.abspath(repo_location),
+        ) as cli_cmd:
+
+        stdout, stderr = cli_cmd.communicate()
+
+    if not stdout:
+        return None
+        
+    tag_unix_date = str(stdout, encoding='utf-8').splitlines()[0]
+
+    if not tag_unix_date:
+        return None
+
+    return datetime.fromtimestamp(int(tag_unix_date))
